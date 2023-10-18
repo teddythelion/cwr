d1d1426c29c576e006d5a9064d8eca1da1292cf8
unknown <tdeleon@ai-codelabs.com>
2023-10-18 14:24:19 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+//console.log("__dirname");
+//console.log(__dirname);
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
